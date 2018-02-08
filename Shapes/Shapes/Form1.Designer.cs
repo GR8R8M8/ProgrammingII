2c25047a00c02c6820a0f8840c1827513fea2d1b
GR8R8M8 <Nickbroza@gmail.com>
2018-02-08 13:12:26 -0500

    Added Shapes

---
+++ Shapes/Shapes/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Shapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(150, 70);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(13, 139);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 384);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Shapes/Shapes/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shapes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        abstract class Shape
+        {
+            public string name { get; set; }
+
+            public Shape(string name = "")
+            {
+                this.name = name;
+            }
+
+            public string GetName()
+            {
+                return this.name;
+            }
+
+            public abstract double getArea();
+
+        }
+
+        class Rectangle : Shape
+        {
+            public double width { get; set; }
+            public double height { get; set; }
+
+            public Rectangle(double width = 0, double height = 0) : base("Rectangle")
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public override double getArea()
+            {
+                return width * height;
+            }
+        }
+
+        class Circle : Shape
+        {
+            public double radius { get; set; }
+            
+            public Circle(double radius) : base("Circle")
+            {
+                this.radius = radius;
+            }
+
+            public override double getArea()
+            {
+                return 2 * Math.PI * radius;
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(5, 10);
+            lblOutput.Text += String.Format("You created a {0} with width {1} and height {2}. It has an area of {3}.\n", rect.GetName(), rect.width, rect.height, rect.getArea());
+
+            Circle circ = new Circle(5);
+            lblOutput.Text += String.Format("You created a {0} with a radius of {1}. It has an area of {2}.\n", circ.GetName(), circ.radius, circ.getArea());
+        }
+    }
+}
