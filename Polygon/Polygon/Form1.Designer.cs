48755a92e13d998e9b052fd40d377e2f64276ec3
GR8R8M8 <Nickbroza@gmail.com>
2018-02-09 11:19:34 -0500

    Uploaded Polygon

---
+++ Polygon/Polygon/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Polygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(50, 69);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(123, 47);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(47, 203);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(228, 80);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 411);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Polygon/Polygon/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Polygon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            ETriangle triangle = new ETriangle(Convert.ToDouble(txtInput.Text));
+            Square square = new Square(Convert.ToDouble(txtInput.Text));
+            Pentagon pentagon = new Pentagon(Convert.ToDouble(txtInput.Text));
+            Hexagon hexagon = new Hexagon(Convert.ToDouble(txtInput.Text));
+            Octagon octagon = new Octagon(Convert.ToDouble(txtInput.Text));
+
+            lblOutput.Text = String.Format("Triangle: {0} \nSquare: {1} \nPentagon: {2} \nHexagon: {3} \nOctagon: {4}", Math.Round(triangle.getArea(), 2), Math.Round(square.getArea(), 2), Math.Round(pentagon.getArea(), 2), Math.Round(hexagon.getArea(), 2), Math.Round(octagon.getArea(), 2));
+
+        }
+
+        #region Polygon
+        abstract class Polygon
+        {
+            protected double side { get; set; }
+
+            public Polygon(double side = 0)
+            {
+                this.side = side;
+            }
+
+            abstract public double getArea();
+        }
+        #endregion
+
+        #region ETriangle : Polygon
+        class ETriangle : Polygon
+        {
+
+            public ETriangle(double side = 0) : base(side) { }
+
+            public override double getArea()
+            {
+                return (Math.Sqrt(3) / 4) * (this.side * this.side);
+            }
+        }
+        #endregion =
+
+        #region Square : Polygon
+        class Square : Polygon
+        {
+            public Square(double side = 0) : base(side) { }
+
+            public override double getArea()
+            {
+                return this.side * this.side;
+            }
+        }
+        #endregion  
+
+        #region Pentagon : Polygon
+        class Pentagon : Polygon
+        {
+            public Pentagon(double side = 0) : base(side) { }
+
+            public override double getArea()
+            {
+                return (.25 * (Math.Sqrt(5 * (5 + (2 * Math.Sqrt(5)))))) * (this.side * this.side);
+            }
+        }
+        #endregion  
+
+        #region Hexagon : Polygon
+        class Hexagon : Polygon
+        {
+            public Hexagon(double side = 0) : base(side) { }
+
+            public override double getArea()
+            {
+                return ((3 * Math.Sqrt(3))/2) * (this.side * this.side);
+            }
+        }
+        #endregion
+
+        #region Octagon : Polygon
+        class Octagon : Polygon
+        {
+            public Octagon(double side = 0) : base(side) { }
+
+            public override double getArea()
+            {
+                return 2 * (1 + Math.Sqrt(2)) * (this.side * this.side);
+            }
+        }
+        #endregion  
+
+    }
+
+
+}
