6d5921ad27ea2ed10f6a5a4c92caea46888983ff
GR8R8M8 <Nickbroza@gmail.com>
2018-02-12 13:19:27 -0500

    Added Students

---
+++ Students/Students/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Students
+{
+    partial class frmStudents
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtGPA = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstDatabase = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(40, 49);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(53, 17);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name: ";
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.AutoSize = true;
+            this.lblGPA.Location = new System.Drawing.Point(47, 78);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(45, 17);
+            this.lblGPA.TabIndex = 1;
+            this.lblGPA.Text = "GPA: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(161, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtGPA
+            // 
+            this.txtGPA.Location = new System.Drawing.Point(91, 75);
+            this.txtGPA.Name = "txtGPA";
+            this.txtGPA.Size = new System.Drawing.Size(161, 22);
+            this.txtGPA.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(78, 132);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(144, 46);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstDatabase
+            // 
+            this.lstDatabase.FormattingEnabled = true;
+            this.lstDatabase.ItemHeight = 16;
+            this.lstDatabase.Location = new System.Drawing.Point(28, 220);
+            this.lstDatabase.Name = "lstDatabase";
+            this.lstDatabase.Size = new System.Drawing.Size(253, 212);
+            this.lstDatabase.TabIndex = 5;
+            // 
+            // frmStudents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 473);
+            this.Controls.Add(this.lstDatabase);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtGPA);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmStudents";
+            this.Text = "Students";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblGPA;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtGPA;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstDatabase;
+    }
+}
+
---
+++ Students/Students/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Students
+{
+
+
+    public partial class frmStudents : Form
+    {
+        public frmStudents()
+        {
+            InitializeComponent();
+        }
+
+        private int databaseFull = 0;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (databaseFull < 5)
+                {
+                    Student student = new Student(txtName.Text, Convert.ToDouble(txtGPA.Text));
+                    lstDatabase.Items.Add(student.ToString());
+                    databaseFull++;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Please enter a valid GPA");
+                txtName.Text = "";
+                txtGPA.Text = "";
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("The number of students is exceeded");
+                txtName.Text = "";
+                txtGPA.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("Fatal error, exiting...");
+                Application.Exit();
+            }
+
+        }
+    }
+
+    public class Student
+    {
+        public string name { get; set; }
+
+        private double _gpa;
+        public double gpa
+        {
+            get { return _gpa; }
+            set
+            {
+                if ((value >= 0) && (value <= 4.00))
+                {
+                    _gpa = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public Student(string name, double gpa)
+        {
+            this.name = name;
+            this.gpa = gpa;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1:F2}", this.name, this.gpa);
+        }
+
+    }
+
+}
