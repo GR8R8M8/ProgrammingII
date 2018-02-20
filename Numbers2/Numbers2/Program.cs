4f13ac5a9f4ffa9fea0b5c0e5c836eee028ea32b
GR8R8M8 <Nickbroza@gmail.com>
2018-02-20 08:08:23 -0500

    Added old work

---
+++ Numbers2/Numbers2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intNum1;
+            int intNum2;
+
+            Console.WriteLine("Enter a value for number 1");
+            intNum1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter a value for number 2");
+            intNum2 = Convert.ToInt16(Console.ReadLine());
+            Sum(intNum1, intNum2);
+            Difference(intNum1, intNum2);
+            Console.WriteLine("The product is:");
+            Console.WriteLine(Product(intNum1, intNum2));
+        }
+
+        static void Sum(int Num1, int Num2)
+        {
+            Console.WriteLine("The sum is:");
+            Console.WriteLine(Num1 + Num2);
+        }
+
+        static void Difference(int Num1, int Num2)
+        {
+            Console.WriteLine("The difference is:");
+            Console.WriteLine(Num1 - Num2);
+        }
+
+        static int Product(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
+
+    }
+}
