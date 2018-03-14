792c1acf9b3616cad8908801eb1c6f31e2fac259
NickBroza <Nickbroza@gmail.com>
2018-03-14 08:31:02 -0400

    Added FileIO Proj II

---
+++ ReadSomeText/ReadSomeText/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteSomeText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filePath = Directory.GetCurrentDirectory() + "/SomeText.txt";
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                string contents = sr.ReadLine();
+
+                Console.WriteLine("The contents are:\n {0}", contents);
+                fs.Close();
+                sr.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found!");
+            }
+            catch
+            {
+                Console.WriteLine("Unhandled exception!");
+            }
+        }
+    }
+}
