f5dfbf1ecc5486b6edbf942e7e319c2a5d5394e2
NickBroza <Nickbroza@gmail.com>
2018-03-20 13:06:39 -0400

    asdjfweokfjdsaipojeiofjasiojdfoi
    added redundant code that is definitely not bad practice

---
+++ GetFolderPaths/GetFolderPaths/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetFolderPaths
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Windows System:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.System));
+            listBox1.Items.Add("Program Files:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
+            listBox1.Items.Add("Desktop Folder:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            listBox1.Items.Add("Favorites:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.Favorites));
+            listBox1.Items.Add("History:\t\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.History));
+
+            listBox1.Items.Add("My Documents:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            listBox1.Items.Add("Programs:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.Programs));
+            listBox1.Items.Add("Recent Files:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.Recent));
+            listBox1.Items.Add("Send To Folder:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.SendTo));
+            listBox1.Items.Add("Start Menu:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.StartMenu));
+
+            listBox1.Items.Add("Start Files:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+            listBox1.Items.Add("System Files:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.System));
+            listBox1.Items.Add("Application Files:\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            listBox1.Items.Add("Common Application Files:\t" + Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData));
+            listBox1.Items.Add("Local Application Files:\t" + Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+
+            listBox1.Items.Add("Cookies:\t\t\t" + Environment.GetFolderPath(Environment.SpecialFolder.Cookies));
+
+        }
+    }
+}
---
+++ ReadEnvironment/ReadEnvironment/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadEnvironment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OperatingSystem opSys = Environment.OSVersion;
+            PlatformID opSysID = opSys.Platform;
+
+            string[] logicalDrives = Environment.GetLogicalDrives();
+            string driveString = "";
+            foreach (string drive in logicalDrives)
+            {
+                driveString += drive + " ";
+            }
+
+            listBox1.Items.Add("Operating System:\t\t"+ opSys);
+            listBox1.Items.Add("Operating SYstem ID:\t" + opSysID);
+            listBox1.Items.Add("Common Language Runtime:\t" + Environment.Version);
+            listBox1.Items.Add("MachineName:\t\t" + Environment.MachineName);
+            listBox1.Items.Add("");
+            listBox1.Items.Add("User ID:\t\t\t" + Environment.UserName);
+            listBox1.Items.Add("Current Directory:\t\t" + Environment.CurrentDirectory);
+            listBox1.Items.Add("Drives Present:\t\t" + driveString);
+
+        }
+    }
+}
