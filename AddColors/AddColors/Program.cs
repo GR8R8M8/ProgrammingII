0b7505694b31d6221298c8142c42f38882840927
NickBroza <Nickbroza@gmail.com>
2018-03-28 09:05:27 -0400

    Upload3

---
+++ AddColors/AddColors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> color1 = new List<string>() {"blue", "pink", "purple"};
+            List<string> color2 = new List<string>() {"navy", "violet"};
+
+            color1.AddRange(color2);
+
+            foreach (string col in color1)
+            {
+                Console.WriteLine(col);
+            }
+        }
+    }
+}
