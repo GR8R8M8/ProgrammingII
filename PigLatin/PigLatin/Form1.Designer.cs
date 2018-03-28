e94d0666c18a3ed6cbf8a3cf4d7a1ba1f1f068d0
NickBroza <Nickbroza@gmail.com>
2018-03-28 09:07:04 -0400

    Added Pig Latin

---
+++ PigLatin/PigLatin/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PigLati
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDo = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(215, 50);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(138, 40);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "Convert";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(95, 124);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(376, 22);
+            this.txtText.TabIndex = 1;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(23, 167);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 368);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnDo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ PigLatin/PigLatin/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigLati
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            string sentence = txtText.Text;
+            List<string> words = new List<string>(sentence.Split());
+            StringBuilder sb = new StringBuilder();
+            foreach (string word in words)
+            {
+                sb.Append(PigConvert(word) + " ");
+            }
+
+            lblOutput.Text = sb.ToString();
+        }
+
+        public string PigConvert(string word)
+        {
+            string result = string.Empty;
+            string pigSuffixVowelFirst = "yay";
+            string pigSuffixConstanantsFirst = "ay";
+
+            string vowels = "aeiouAEIOU";
+
+            if (vowels.Contains(word.First()))
+            {
+                result = word + pigSuffixVowelFirst;
+            }
+            else
+            {
+                int count = 0;
+                string end = string.Empty;
+                foreach (char c in word)
+                {
+                    if (!vowels.Contains(c))
+                    {
+                        end += c;
+                        count++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                result = word.Substring(count) + end + pigSuffixConstanantsFirst;
+            }
+            return result;
+        }
+
+
+
+
+    }
+}
