0c313872fce5bd7fa16d598891b03de5cb2b02d0
NickBroza <Nickbroza@gmail.com>
2018-04-10 13:24:40 -0400

    Added string stuff

---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text: ");
+            string words = Console.ReadLine();
+
+            Console.WriteLine("Enter your shift: ");
+            int shift = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Encrypted Text: ");
+            Console.WriteLine(Encrypt(words, shift));
+        }
+
+        public static string Encrypt(string words, int shift)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in words)
+                output += Move(ch, shift);
+
+            return output;
+        }
+
+        private static char Move(char ch, int shift)
+        {
+            if (!char.IsLetter(ch))
+                return ch;
+
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + shift) - offset) % 26) + offset);
+        }
+
+
+    }
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public enum PalindromeType {Mirrored, Pivot, Neither};
+
+        static void Main(string[] args)
+        {
+            string word = null;
+            int M = 0;
+            int P = 0;
+            int N = 0;
+
+            do
+            {
+                Console.WriteLine("Enter the word: ");
+                word = Console.ReadLine();
+
+                switch (Check(word))
+                {
+                    case PalindromeType.Mirrored:
+                        Console.WriteLine("Mirrored");
+                        M++;
+                        break;
+
+                    case PalindromeType.Pivot:
+                        Console.WriteLine("Pivot");
+                        P++;
+                        break;
+
+                    case PalindromeType.Neither:
+                        Console.WriteLine("Neither!");
+                        N++;
+                        break;
+                }
+            } while (word != "QQQ");
+
+            Console.WriteLine("Mirrored: " + M);
+            Console.WriteLine("Pivot: " + P);
+            Console.WriteLine("Neither: " + N);
+        }
+
+        static PalindromeType Check(string str)
+        {
+            str.First().ToString().ToLower();
+            string strRev = new string(str.ToCharArray().Reverse().ToArray());
+
+            if (str == strRev && str.Length % 2 == 0)
+            {
+                return PalindromeType.Mirrored;
+            }
+
+            if (str == strRev && str.Length % 2 > 0)
+            {
+                return PalindromeType.Pivot;
+            }
+
+            return PalindromeType.Neither;
+        }
+    }
+}
