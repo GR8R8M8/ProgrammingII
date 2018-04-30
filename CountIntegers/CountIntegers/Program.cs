183c1d537410cca78952acea1a54913c464b3493
NickBroza <Nickbroza@gmail.com>
2018-04-30 12:38:28 -0400

    added some spaghetti code that should work (set 1)

---
+++ CountIntegers/CountIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            FileStream fs = new FileStream("res/testRead.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string n = sr.ReadLine();
+            fs.Close();
+            sr.Close();
+
+            List<int> num = new List<int>(n.Split((string[])null, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList());
+
+            var query = from nt in num
+                where nt == s
+                select nt;
+
+            for (int i = 0; i < num.Max(); i++)
+            {
+                s = i;
+                Console.WriteLine("Key " + i + " occured " + query.Count() + " times");
+            }
+
+        }
+    }
+}
---
+++ IntegerSquares/IntegerSquares/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a set of numbers on one line");
+            string l = Console.ReadLine();
+
+            List<int> num = new List<int>(l.Split((string[]) null, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList());
+
+            var sn = from int n in num
+                let sqn = n * n
+                select sqn;
+
+            Console.WriteLine("Squared numbers:");
+            foreach (int n in sn)
+            {
+                Console.Write(n + " ");
+            }
+
+        }
+    }
+}
