a4632290476de57d8afa2df7a505aadd1f67ef4b
NickBroza <Nickbroza@gmail.com>
2018-04-30 12:39:28 -0400

    last set of spaghetti (3)

---
+++ TeamMaker/TeamMaker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sortedNames = new string[] {"Nick", "Pierce", "Daniel", "Tyler", "Sydney", "Chris", "Kush", "Jordan", "Taylor", "Shamyl", "Jake", "Cole"};
+
+            Random rnd = new Random();
+            string[] randomNames = sortedNames.OrderBy(x => rnd.Next()).ToArray();
+
+            var teams = from name in randomNames
+                let i = Array.IndexOf(randomNames, name) + 1
+                where  i % 3 == 0
+                select (" " + name + " " + randomNames[i - 2] + " " + randomNames[i - 3]);
+
+            foreach (var team in teams)
+            {
+                Console.WriteLine("Team: " + team);
+            }
+
+        }
+    }
+}
---
+++ TopN/TopN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a set of numbers on one line");
+            string l = Console.ReadLine();
+            Console.WriteLine("Enter the number of top numbers");
+            int am = Convert.ToInt16(Console.ReadLine());
+
+            List<int> num = new List<int>(l.Split((string[])null, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList());
+            num.Sort();
+
+            var s = from n in num
+                where n >= num[num.Count - am]
+                select n;
+
+            Console.WriteLine("Top Numbers: ");
+            foreach (var n in s)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
