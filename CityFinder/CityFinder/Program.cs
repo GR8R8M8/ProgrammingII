630c04379cdba265d86865f6ad9409541c110042
NickBroza <Nickbroza@gmail.com>
2018-04-30 12:39:02 -0400

    added spaghetti code (2)

---
+++ CityFinder/CityFinder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>{"Rome","London","Nairobi","Zurich","Los Angeles","New Delhi","Amsterdam","Abu Dhabi","Paris","Anaheim"};
+
+            var search = from c in cities
+                where c.First().ToString().ToUpper() == "A" && c.Last().ToString().ToUpper() == "M"
+                select c;
+
+            foreach (var c in search)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ Gettysburg/Gettysburg/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gettysburg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = File.ReadAllText("res/Gettysburg.txt");
+
+            List<string> words = new List<string>(address.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' , '-' }, StringSplitOptions.RemoveEmptyEntries));
+
+            var query = from word in words
+                where word == "we"
+                select word;
+
+
+            Console.WriteLine(query.Count());
+
+        }
+    }
+}
